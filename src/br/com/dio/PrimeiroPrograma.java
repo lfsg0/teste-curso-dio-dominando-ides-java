cbe11b3563d46c8f1917ea80d5f80bcd183e7164
Luana <lfsg.22@outlook.com>
2022-07-13 22:39:24 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,14 +6,15 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 	
-		Gato gato = new Gato();	
+		Gato gato = new Gato();
 		Livros livros = new Livros();
-		System.out.println(gato);
 		System.out.println(livros);
+		System.out.println(gato);
+		
 	}
  
 }	
-
+ 
 class Livros {
 	private String nome;
 	private String nPag;
--- src/br/com/dio/model/Gato.java
+++ src/br/com/dio/model/Gato.java
@@ -1,47 +1,41 @@
 package br.com.dio.model;
 
 import java.util.Objects;
-public class Gato { 
+
+public class Gato {
 	
 	private String nome;
 	private String cor;
 	private Integer idade;
-	
+	public Gato() {
+	}
 	public Gato(String nome, String cor, Integer idade) {
 		this.nome = nome;
 		this.cor = cor;
 		this.idade = idade;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getCor() {
 		return cor;
 	}
-
 	public void setCor(String cor) {
 		this.cor = cor;
 	}
-
 	public Integer getIdade() {
 		return idade;
 	}
-
 	public void setIdade(Integer idade) {
 		this.idade = idade;
 	}
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(cor, idade, nome);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -53,15 +47,15 @@ public class Gato {
 		Gato other = (Gato) obj;
 		return Objects.equals(cor, other.cor) && Objects.equals(idade, other.idade) && Objects.equals(nome, other.nome);
 	}
-
 	@Override
 	public String toString() {
 		return "Gato [nome=" + nome + ", cor=" + cor + ", idade=" + idade + "]";
 	}
 	
-
+	
+	
+	
+	
+	
 	
 }
-
-	
-
